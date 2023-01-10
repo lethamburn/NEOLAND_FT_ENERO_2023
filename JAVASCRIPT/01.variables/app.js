601251d81cf0af438c9ec8907085ffad744c4a06
Antonio Rosales Martínez <antoniorosalesmartinez@MacBook-Pro-de-Antonio.local>
2023-01-10 09:48:34 +0100

    feat 01.variables

---
+++ JAVASCRIPT/01.variables/app.js
@@ -0,0 +1,45 @@
+//VARIABLE
+let x = 10;
+//CONSTANTE
+const y = 12;
+
+//Declarar una variable / constante
+let car;
+
+//Asignar un valor
+car = "BMW";
+
+//Las constantes tienen que tener un valor asignado
+const bike = "Toyota";
+
+//Declarar y asignar valor a varias constantes o variables
+const numA = 3;
+numB = 4;
+
+//Variables
+let numC = 6;
+console.log("El valor de numC en la linea 21 es:", numC);
+numC = 8;
+console.log("El valor de numC en la linea 23 es:", numC);
+
+//Constantes
+const numD = 5;
+//numD = 6;
+
+//COMENTARIOS
+/* 
+A
+B
+C
+ */
+
+//NAMING - NO SE PUEDEN NUMEROS NI GUIONES MEDIOS NI NOMBRES AJENOS AL CONTENIDO
+
+const thing = "Peter Parker";
+//const full-name = "Peter Parker"
+const peterParker = "Peter Paker";
+
+//camelCase
+const fullName = "Peter Parker";
+//Barra baja
+const full_name = "Peter Parker";
